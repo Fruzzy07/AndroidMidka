9a0b2a5d53c7a493bfe87ed75c1aa1f460fcdfb3
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2024-03-09 19:44:50 +0500

    OfferListAdapter.kt

--- app/src/main/java/com/example/aviatickets/adapter/OfferListAdapter.kt
+++ app/src/main/java/com/example/aviatickets/adapter/OfferListAdapter.kt
@@ -1,17 +1,24 @@
 package com.example.aviatickets.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.aviatickets.R
 import com.example.aviatickets.databinding.ItemOfferBinding
 import com.example.aviatickets.model.entity.Offer
 
-class OfferListAdapter : ListAdapter<Offer, OfferListAdapter.ViewHolder>(OfferDiffCallback()) {
+class OfferListAdapter : RecyclerView.Adapter<OfferListAdapter.ViewHolder>() {
 
     private val items: ArrayList<Offer> = arrayListOf()
+
+    fun setItems(offerList: List<Offer>) {
+        items.clear()
+        items.addAll(offerList)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -31,6 +38,16 @@ class OfferListAdapter : ListAdapter<Offer, OfferListAdapter.ViewHolder>(OfferDi
         holder.bind(items[position])
     }
 
+    fun sortByPrice() {
+        items.sortBy { it.price }
+        notifyDataSetChanged()
+    }
+
+    fun sortByDuration() {
+        items.sortBy { it.flight.duration }
+        notifyDataSetChanged()
+    }
+
     inner class ViewHolder(
         private val binding: ItemOfferBinding
     ) : RecyclerView.ViewHolder(binding.root) {
@@ -41,6 +58,9 @@ class OfferListAdapter : ListAdapter<Offer, OfferListAdapter.ViewHolder>(OfferDi
             val flight = offer.flight
 
             with(binding) {
+                Glide.with(itemView)
+                    .load(flight.airline.logoUrl) // Replace with the actual URL or resource identifier
+                    .into(airlineLogoImageView)
                 departureTime.text = flight.departureTimeInfo
                 arrivalTime.text = flight.arrivalTimeInfo
                 route.text = context.getString(
@@ -62,16 +82,26 @@ class OfferListAdapter : ListAdapter<Offer, OfferListAdapter.ViewHolder>(OfferDi
             first = minutes / 60,
             second = minutes % 60
         )
+    }
+}
 
+class OfferDiffCallback : DiffUtil.ItemCallback<Offer>() {
+    override fun areItemsTheSame(oldItem: Offer, newItem: Offer): Boolean {
+        return oldItem.id == newItem.id
     }
 
-    private class OfferDiffCallback : DiffUtil.ItemCallback<Offer>() {
-        override fun areItemsTheSame(oldItem: Offer, newItem: Offer): Boolean {
-            return oldItem.id == newItem.id
-        }
+    override fun areContentsTheSame(oldItem: Offer, newItem: Offer): Boolean {
+        return oldItem == newItem
+    }
 
-        override fun areContentsTheSame(oldItem: Offer, newItem: Offer): Boolean {
-            return oldItem == newItem
+    override fun getChangePayload(oldItem: Offer, newItem: Offer): Any? {
+        val diffBundle = Bundle()
+        if (oldItem.price != newItem.price) {
+            diffBundle.putDouble("price", newItem.price.toDouble())
         }
+        if (oldItem.flight.duration != newItem.flight.duration) {
+            diffBundle.putInt("duration", newItem.flight.duration)
+        }
+        return if (diffBundle.isEmpty) null else diffBundle
     }
 }
