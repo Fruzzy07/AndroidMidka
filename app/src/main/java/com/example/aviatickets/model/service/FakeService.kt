ae81d9bafcac759a7bdfe9ebf3dda4547196df08
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2024-03-09 19:50:31 +0500

    FakeService.kt

--- app/src/main/java/com/example/aviatickets/model/service/FakeService.kt
+++ app/src/main/java/com/example/aviatickets/model/service/FakeService.kt
@@ -26,7 +26,8 @@ object FakeService {
                 flightNumber = "981",
                 airline = Airline(
                     name = "Air Astana",
-                    code = "KC"
+                    code = "KC",
+                    logoUrl = "https://avatars.mds.yandex.net/i?id=68fbf3d772010d0264b5ac697f097eb93e96dd20-10703102-images-thumbs&n=13"
                 ),
                 duration = 120
             )
@@ -48,7 +49,8 @@ object FakeService {
                 flightNumber = "991",
                 airline = Airline(
                     name = "Air Astana",
-                    code = "KC"
+                    code = "KC",
+                    logoUrl = "https://avatars.mds.yandex.net/i?id=68fbf3d772010d0264b5ac697f097eb93e96dd20-10703102-images-thumbs&n=13"
                 ),
                 duration = 120
             )
@@ -70,7 +72,8 @@ object FakeService {
                 flightNumber = "445",
                 airline = Airline(
                     name = "FlyArystan",
-                    code = "KC"
+                    code = "KC",
+                    logoUrl = "https://cdn.nur.kz/images/1200x675/5a2119bb0eb0649f.jpeg?version=1"
                 ),
                 duration = 100
             )
@@ -92,7 +95,8 @@ object FakeService {
                 flightNumber = "223",
                 airline = Airline(
                     name = "SCAT Airlines",
-                    code = "DV"
+                    code = "DV",
+                    logoUrl = "https://avatars.mds.yandex.net/i?id=f6f32c04264e74d71cc7c3ab4a1341edd4ac8629-12496607-images-thumbs&n=13"
                 ),
                 duration = 90
             )
@@ -114,7 +118,8 @@ object FakeService {
                 flightNumber = "171",
                 airline = Airline(
                     name = "QazaqAir",
-                    code = "IQ"
+                    code = "IQ",
+                    logoUrl = "https://sun1-17.userapi.com/s/v1/if1/2JcXOGBXu9Ap3EAYoqDexW2yw5FPcBEIlwvvVafpj4SJZqmQriZ0r6B9tpymT15wqnP8Zw.jpg?size=1535x1535&quality=96&crop=0,0,1535,1535&ava=1"
                 ),
                 duration = 135
             )
